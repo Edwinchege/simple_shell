1e1682a6b88dc5a3ad7ab942ec9bbddeaaa96d09
Edwinchege <edwinchegee@gmail.com>
2023-10-17 09:35:12 -0700

    Shell_Project_Update

--- mycd.c
+++ mycd.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
-* _mycd - changes the current directory within the program
-* @info: address to the structure
-*  Return: Always 0
-*/
+ * _mycd - changes the current directory within the program
+ * @info: address to the structure
+ *  Return: Always 0
+ */
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
@@ -12,22 +12,17 @@ int _mycd(info_t *info)
 
 	s = getcwd(buffer, 1024);
 	if (!s)
-	{
 		_puts("TODO: >>getcwd failure emsg here<<\n");
-	}
 	if (!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
 		if (!dir)
-		{
 			chdir_ret =
-		}
-		chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
-		{
 			chdir_ret = chdir(dir);
-		}
-	} else if (_strcmp(info->argv[1], "-") == 0)
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
@@ -37,15 +32,16 @@ int _mycd(info_t *info)
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret =
-		chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	} else
+		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
 	} else
 	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD = "));
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
